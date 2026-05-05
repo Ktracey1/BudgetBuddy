d3f090fb3871189e337f22f067b6f6462e874ebd
Glorious <glriousgbeb@gmail.com>
2026-05-05 19:50:58 -0400

    Fixed error with middleware

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Item = require("../models/Item");
+const auth = require("../middleware/authMiddleware");
 
 // CREATE
 router.post("/add", async (req, res) => {
